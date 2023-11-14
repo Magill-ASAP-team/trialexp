187517278d971337d2f6ef87acc9985110f8fed6
Teris Tam <teristam@gmail.com>
2023-11-14 14:25:44 +0000

    add new plot type

--- trialexp/process/figures/plot_utils.py
+++ trialexp/process/figures/plot_utils.py
@@ -1,4 +1,6 @@
+from scipy.cluster.hierarchy import dendrogram
 import matplotlib.pylab as plt 
+import numpy as np
 
 def create_plot_grid(n_plots, ncol, figsize_plot=(3,3), **kwargs):
     nrow = n_plots//ncol + 1
@@ -9,3 +11,24 @@ def create_plot_grid(n_plots, ncol, figsize_plot=(3,3), **kwargs):
         axes.append(ax)
     return fig, axes
 
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
