21703c08b0ae271d834c2ec5c7ae3da192ba55c3
Teris Tam <teristam@gmail.com>
2024-10-08 11:41:36 +0100

    update env name

--- cron_run.sh
+++ cron_run.sh
@@ -5,7 +5,7 @@ source /home/MRC.OX.AC.UK/ndcn1330/miniconda3/etc/profile.d/conda.sh
 conda activate trialexp2
 cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/
 git clone --depth 1 https://github.com/Magill-ASAP-team/trialexp.git 
-conda activate trialexp2
+conda activate trialexp
 cd trialexp
 python workflow/scripts/00_create_session_folders.py
 snakemake --snakefile workflow/pycontrol.smk -k -c20 --rerun-triggers mtime -q progress
