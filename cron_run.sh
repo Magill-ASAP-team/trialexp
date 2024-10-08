56cd06d8db9ebd86ed6c333ef2cb82900a4eca0f
Teris Tam <teristam@gmail.com>
2024-10-08 11:42:38 +0100

    bugfixx

--- cron_run.sh
+++ cron_run.sh
@@ -2,7 +2,7 @@
 echo "========Running pipeline==========="
 echo $(date)
 source /home/MRC.OX.AC.UK/ndcn1330/miniconda3/etc/profile.d/conda.sh
-conda activate trialexp2
+conda activate trialexp
 cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/
 git clone --depth 1 https://github.com/Magill-ASAP-team/trialexp.git 
 conda activate trialexp
