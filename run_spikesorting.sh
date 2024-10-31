39c385fe4939549a84e0ee0e3cfe8534c7b7afba
Teris Tam <teristam@gmail.com>
2024-10-31 18:16:52 +0000

    update requirements and env name

--- run_spikesorting.sh
+++ run_spikesorting.sh
@@ -2,7 +2,7 @@
 
 # Activate the conda environment
 source /home/MRC.OX.AC.UK/ndcn1330/miniconda3/etc/profile.d/conda.sh
-conda activate trialexp2
+conda activate trialexp
 
 # Run the Python file
 snakemake --snakefile workflow/spikesort.smk -k -c5 --rerun-triggers mtime
