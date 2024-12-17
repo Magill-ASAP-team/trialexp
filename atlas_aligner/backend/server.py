19405c5b4d3acf36af657135fc878e6d21c22ee1
Teris Tam <teristam@gmail.com>
2024-12-17 18:05:22 +0000

    WIP

--- atlas_aligner/backend/server.py
+++ atlas_aligner/backend/server.py
@@ -1,9 +1,12 @@
+#%%
 import fastapi
 from enum import Enum
 from typing import Optional
 from trialexp.process.folder_org.utils import build_session_info_cohort
 from fastapi.middleware.cors import CORSMiddleware
-
+from pathlib import Path
+import numpy as np
+#%%
 root_path = '/mnt/Magill_Lab/Julien/ASAP/Data'
 df_session_info = build_session_info_cohort(root_path)
 
@@ -54,3 +57,29 @@ async def get_animal_id(cohort: Optional[str] = None):
 
 
 #%%
+
+root_path = '/mnt/Magill_Lab/Julien/ASAP/Data'
+df_session_info = build_session_info_cohort(root_path)
+# %%
+df_sesssion_info = df_session_info.query('neuropixels_sorted==True')
+#%%
+df_sel = df_session_info.query('animal_id=="TT011"').iloc[0]
+# %%
+# match with the track localization results
+local_results = Path(root_path)/df_sel.cohort/'histology'/df_sel.animal_id/'RGB'/'Processed'
+# %%
+import scipy.io as sio
+probe_ccf = sio.loadmat(str(local_results/'probe_ccf.mat'), simplify_cells=True)['probe_ccf']
+# %%
+probe_ccf[0]['trajectory_areas']
+# %%
+# trajectory coords is in ap, dv, ml
+# ref see https://community.brain-map.org/t/how-to-transform-ccf-x-y-z-coordinates-into-stereotactic-coordinates/1858
+coords = probe_ccf[0]['trajectory_coords']
+
+#%% atlas
+# The annoatation volume is a 3D volume with an index at each voxel
+# it present the row of in the structure_tree_safe_2017 table
+# the template volume contains the reference image of the brain (voxel with each element the brightness of the pixel)
+atlas = np.memmap('/mnt/Magill_Lab/Julien/ASAP/software/allenccf/annotation_volume_10um_by_index.npy')
+# %%
