21988b4329c50ffd460e1e5186bb38aee28fcf1d
Teris Tam <teristam@gmail.com>
2024-12-17 15:58:41 +0000

    Add session search boxes

--- atlas_aligner/atlas_aligner/src/App.tsx
+++ atlas_aligner/atlas_aligner/src/App.tsx
@@ -1,25 +1,85 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '@mantine/core/styles.css'
 import { createTheme, MantineProvider } from '@mantine/core'
 import { Button, Select } from '@mantine/core'
 import { Flex } from '@mantine/core'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [cohort, setCohort] = useState(['August','April'])
-  const [animalID, setAnimalID] = useState(['A','B','C'])
-  const [sessionID, setSessionID] = useState(['1','2','3'])  
+  const [cohortList, setCohortList] = useState(['August', 'April'])
+  const [animalIDList, setAnimalIDList] = useState(['A', 'B', 'C'])
+  const [sessionIDList, setSessionIDList] = useState(['1', '2', '3'])
+  const [cohort, setCohort] = useState<string | null>(null)
+  const [animalID, setAnimalID] = useState<string | null>(null)
+  const [sessionID, setSessionID] = useState<string | null>(null)
+
+  useEffect(() => {
+    // a async function to fetch data
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/cohorts')
+        if (!response.ok) {
+          throw new Error('Server error');
+        }
+        const result = await response.json();
+        setCohortList(result['cohorts'])
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    // a async function to fetch data
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/animal_id?cohort=${cohort}`)
+        if (!response.ok) {
+          throw new Error('Server error');
+        }
+        const result = await response.json();
+        setAnimalID(null);
+        setAnimalIDList(result['animal_id']);
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    }
+
+    fetchData();
+  }, [cohort]);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/sessions', 
+      { params: { cohort: cohort, animal_id: animalID } })
+      .then((response) => {
+        setSessionIDList(null);
+        setSessionIDList(response.data['session_id']);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+      
+  }, [cohort, animalID]);
+
+
 
   return (
     <>
       <MantineProvider>
         <Flex justify="center" align="center" gap="md">
-          <Button> Hello, mantine! </Button>
-
-          <Select data={cohort} placeholder="Select your favorite framework" />
-          <Select data={animalID} placeholder="Select your favorite framework" />
-          <Select data={sessionID} placeholder="Select your favorite framework" />
+          <Select data={cohortList} placeholder="Cohort"
+            label='Cohort'
+            value={cohort} onChange={setCohort} searchable />
+          <Select data={animalIDList} placeholder="Animal" label='Animal'
+            value={animalID} onChange={setAnimalID} searchable/>
+          <Select data={sessionIDList} placeholder="Session" label='Session'
+            value={sessionID} onChange={setSessionID} searchable />
 
         </Flex>
       </MantineProvider>
--- atlas_aligner/backend/server.py
+++ atlas_aligner/backend/server.py
@@ -2,24 +2,22 @@ import fastapi
 from enum import Enum
 from typing import Optional
 from trialexp.process.folder_org.utils import build_session_info_cohort
+from fastapi.middleware.cors import CORSMiddleware
 
 root_path = '/mnt/Magill_Lab/Julien/ASAP/Data'
 df_session_info = build_session_info_cohort(root_path)
-# df_session_info = df_session_info.query("animal_id=='TT013'")
 
 
 app = fastapi.FastAPI()
-app.state.shared_obj = {'df_session_info': df_session_info}
+app.state.df_session_info = df_session_info
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/{model_name}")
-async def get_model(model_name:ModelName):
-    return {"message": model_name}
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to the specific origins you want to allow
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/sessions/')
 async def get_sessions(cohort: Optional[str] = None, animal_id: Optional[str] = None):
@@ -29,11 +27,26 @@ async def get_sessions(cohort: Optional[str] = None, animal_id: Optional[str] = 
         query_str += f"cohort=='{cohort}'"
 
     if animal_id:
-        query_str += f'animal_id=="{animal_id}"'
+        query_str += f'and animal_id=="{animal_id}"'
     
-    df = app.state.shared_obj['df_session_info']
+    df = app.state.df_session_info
     
     if len(query_str) > 0:
         df = df.query(query_str)
         
-    return {"message": df.session_id.to_list()}
+    print(type(df.session_id.unique().tolist()))
+        
+    return {"session_id": df.session_id.unique().tolist()}
+
+@app.get('/cohorts')
+async def get_cohort():
+    df = app.state.df_session_info
+    return {"cohorts": df.cohort.unique().tolist()}
+
+
+@app.get('/animal_id')
+async def get_animal_id(cohort: Optional[str] = None):
+    df = app.state.df_session_info
+    if cohort:
+        df = df.query(f"cohort=='{cohort}'")
+    return {"animal_id": df.animal_id.unique().tolist()}
