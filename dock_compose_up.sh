405d0ba3e2961225ab71a28ba59b87ccf84753fb
Teris Tam <teristam@gmail.com>
2025-03-04 11:14:39 +0000

    bugfix: env file name

--- dock_compose_up.sh
+++ dock_compose_up.sh
@@ -5,7 +5,7 @@ MACHINE_NAME=$(hostname)
 if [ MACHINE_NAME = "jade" ]; then
     docker-compose --env-file linux.env up
 elif [ MACHINE_NAME = "lapis" ]; then
-    docker-compose --env-file linx_lapis up
+    docker-compose --env-file linx_lapis.env up
 else
     echo "Usage: ./run.sh [machine_a|machine_b]"
 fi
