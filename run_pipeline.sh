218b4e2f4f510110b913f293f36cb4ba96836ad2
Teris Tam <teristam@gmail.com>
2025-06-04 18:16:55 +0100

    use justfile to execute the pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Activate the conda environment
-source /home/MRC.OX.AC.UK/ndcn1330/miniconda3/etc/profile.d/conda.sh
-conda activate trialexp
 
 # Run the Python file
-python workflow/scripts/00_create_session_folders.py
-
-snakemake --snakefile workflow/pycontrol.smk -k -c20 --rerun-triggers mtime
+just run-pipeline
