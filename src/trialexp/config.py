81a3c2c2d2ca4e255f59f67c58d66ebbad4d6f9d
Teris Tam <teristam@gmail.com>
2025-06-04 18:40:45 +0100

    Move the tools into one folder

---
+++ src/trialexp/config.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from trialexp.process.pycontrol.utils import auto_load_dotenv
+
+
+# Load environment variables from .env file
+auto_load_dotenv()
+
+
+# Pre-load environment variables from .env file
+SNAKEMAKE_DEBUG_ROOT = os.environ.get('SNAKEMAKE_DEBUG_ROOT')
+ETTIN_MOUNT_PATH = os.environ.get('ETTIN_MOUNT_PATH')
+SESSION_ROOT_DIR = os.environ.get('SESSION_ROOT_DIR')
+ETTIN_DATA_FOLDER = os.environ.get('ETTIN_DATA_FOLDER')
+
+# Deeplabcut
+VIDEO_DIR = os.environ.get('VIDEO_DIR')
+DLC_CONFIG_PATH = os.environ.get('DLC_CONFIG_PATH')
+
+# Sorting
+SORTING_ROOT_DATA_PATH = os.environ.get('SORTING_ROOT_DATA_PATH')
+CODE_ROOT_FOLDER = os.environ.get('CODE_ROOT_FOLDER')
+KILOSORT3_PATH = os.environ.get('KILOSORT3_PATH')
+CORTEX_LAB_SPIKES_PATH = os.environ.get('CORTEX_LAB_SPIKES_PATH')
+NPY_MATLAB_PATH = os.environ.get('NPY_MATLAB_PATH')
+CELL_EXPLORER_PATH = os.environ.get('CELL_EXPLORER_PATH')
+RAW_DATA_ROOT_DIR = os.environ.get('RAW_DATA_ROOT_DIR')
+ANATOMY_ROOT_DIR = os.environ.get('ANATOMY_ROOT_DIR')
+TEMP_DATA_PATH = os.environ.get('TEMP_DATA_PATH')
+
--- workflow/scripts/settings.py
+++ workflow/scripts/settings.py
@@ -4,22 +4,23 @@ import dotenv
 from trialexp.process.pycontrol.utils import auto_load_dotenv
 from pathlib import Path
 from glob import glob
+from trialexp import config
 #%%
 
 # make sure we load the .env file from the correct location
-cwd = Path(os.getcwd())
-if cwd.name == 'scripts':
-    auto_load_dotenv(str(cwd.parents[1]))
-if cwd.parent.name == 'scripts':
-    auto_load_dotenv(str(cwd.parents[2]))
-else:
-    auto_load_dotenv()
+# cwd = Path(os.getcwd())
+# if cwd.name == 'scripts':
+#     auto_load_dotenv(str(cwd.parents[1]))
+# if cwd.parent.name == 'scripts':
+#     auto_load_dotenv(str(cwd.parents[2]))
+# else:
+#     auto_load_dotenv()
 
 # debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms063-2023-02-27-164426/'
 # debug_folder = r'//home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/_Other/test_folder_ephys/kms058_2023-03-24_15-09-44_bar_final/Record Node 101/experiment1/recording1/continuous/Neuropix-PXI-100.ProbeA-AP/sorting/'
 
 session_id = 'TT019-2025-05-30-112229'
-debug_folder = list(Path(os.environ['SESSION_ROOT_DIR']).glob(f'*/by_sessions/*/{session_id}'))[0]
+debug_folder = list(Path(config.SESSION_ROOT_DIR).glob(f'*/by_sessions/*/{session_id}'))[0]
 debug_folder = str(debug_folder)
 # debug_folder = r'/home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/by_sessions/reaching_go_spout_bar_nov22/kms062-2023-03-06-182344'
 # %%
