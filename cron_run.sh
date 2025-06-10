1b673924919f4c3829fc99b557a016b3a0e960d3
Teris Tam <teristam@gmail.com>
2025-06-10 17:05:00 +0100

    modify script to work with uv

--- cron_run.sh
+++ cron_run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "========Running pipeline==========="
-echo $(date)
-source /home/MRC.OX.AC.UK/ndcn1330/miniconda3/etc/profile.d/conda.sh
-conda activate trialexp
-cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/
-git clone --depth 1 https://github.com/Magill-ASAP-team/trialexp.git 
-conda activate trialexp
-cd trialexp
-python workflow/scripts/00_create_session_folders.py
-snakemake --snakefile workflow/pycontrol.smk -k -c20 --rerun-triggers mtime -q progress
-cp -rf /home/MRC.OX.AC.UK/ndcn1330/code/tmp/trialexp/.snakemake/* /home/MRC.OX.AC.UK/ndcn1330/code/trialexp/.snakemake
-rm -rf /home/MRC.OX.AC.UK/ndcn1330/code/tmp/trialexp
+cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/trialexp
+git pull
+uv sync
+just 
