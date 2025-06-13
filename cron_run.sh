b90a0c51817ef68a82d81e43d765e81f13c25392
Teris Tam <teristam@gmail.com>
2025-06-13 18:03:47 +0100

    bugfix

--- cron_run.sh
+++ cron_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+source .venv/bin/activate
 cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/trialexp
 git pull
-/home/MRC.OX.AC.UK/ndcn1330/.local/bin/uv sync
-/home/MRC.OX.AC.UK/ndcn1330/code/trialexp/.venv/bin/just 
+uv sync
+just 
