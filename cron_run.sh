cb004f87b9a4c8bb19e2b1c581738376789b6892
Teris Tam <teristam@gmail.com>
2025-06-13 18:14:01 +0100

    fix

--- cron_run.sh
+++ cron_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/MRC.OX.AC.UK/ndcn1330/code/trialexp
 export PATH="/home/MRC.OX.AC.UK/ndcn1330/.local/bin/:$PATH" #manually add uv to path
+source .venv/bin/activate
 git pull
 uv sync
 just 
