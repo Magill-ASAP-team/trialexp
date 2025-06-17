3776c285641b8fd13841b70744218057fdf6962e
Teris Tam <teristam@gmail.com>
2025-06-17 17:34:15 +0100

    use tmp location for daily execution of pipeline

--- cron_run.sh
+++ cron_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/MRC.OX.AC.UK/ndcn1330/code/trialexp
+cd /home/MRC.OX.AC.UK/ndcn1330/code/tmp/trialexp
 export PATH="/home/MRC.OX.AC.UK/ndcn1330/.local/bin/:$PATH" #manually add uv to path
 source .venv/bin/activate
 git pull
