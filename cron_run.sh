9ffa54aad03f787253d81a9c76f31b6e1b88a09e
Teris Tam <teristam@gmail.com>
2025-06-17 17:32:34 +0100

    add the full sync

--- cron_run.sh
+++ cron_run.sh
@@ -3,5 +3,5 @@ cd /home/MRC.OX.AC.UK/ndcn1330/code/trialexp
 export PATH="/home/MRC.OX.AC.UK/ndcn1330/.local/bin/:$PATH" #manually add uv to path
 source .venv/bin/activate
 git pull
-uv sync
+uv sync --extra full
 just 
