c48d843b0b42383b2ca50c59d818520dd999ddfb
Teris Tam <teristam@gmail.com>
2025-06-18 13:42:40 +0100

    add lick rate signal to all events

--- src/trialexp/config.py
+++ src/trialexp/config.py
@@ -26,6 +26,6 @@ RAW_DATA_ROOT_DIR = os.environ.get('RAW_DATA_ROOT_DIR')
 ANATOMY_ROOT_DIR = os.environ.get('ANATOMY_ROOT_DIR')
 TEMP_DATA_PATH = os.environ.get('TEMP_DATA_PATH')
 
-session_id = 'RE026-2025-06-10-115941'
+session_id = 'TT018-2025-06-17-131805'
 debug_folder = list(Path(SESSION_ROOT_DIR).glob(f'*/by_sessions/*/{session_id}'))[0]
 debug_folder = str(debug_folder)
