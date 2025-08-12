5278a906b7acbddcc040c1d0f44446c1146cf729
Teris Tam <teristam@gmail.com>
2025-08-12 18:48:40 +0100

    bugfix: typo

--- src/trialexp/process/pyphotometry/utils.py
+++ src/trialexp/process/pyphotometry/utils.py
@@ -1533,7 +1533,7 @@ def preprocess_photometry(data_photometry, df_pycontrol):
                 data_photometry = motion_correction_win(data_photometry)
     else:
         # Canont find animal, emit error and stop
-        raise ValueError(f'Error: cannot find animation info for {animal_id}. Aborting.') 
+        raise ValueError(f'Error: cannot find animal info for {animal_id}. Aborting.') 
     
     
     data_photometry = compute_df_over_f(data_photometry, low_pass_cutoff=0.01)
